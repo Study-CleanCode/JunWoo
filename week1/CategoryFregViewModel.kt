ebaafb098b5ff7c181acdeeea0261573ae6e42f2
JunwooKim <rhffldkt0517@naver.com>
2023-04-26 14:58:46 +0900

    Create CategoryFregViewModel.kt

---
+++ week1/CategoryFregViewModel.kt
@@ -0,0 +1,17 @@
+package com.android.hara.presentation.write.fragment.category
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class CategoryFregViewModel @Inject constructor() : ViewModel() {
+    private val _category = MutableLiveData<Int>()
+    val category get() = _category
+
+    fun setCategory(category: Int) {
+        _category.value = category
+    }
+}
+
