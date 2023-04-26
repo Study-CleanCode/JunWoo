9d113e19e1f5abc23d12b560ceb954e5ad457396
JunwooKim <rhffldkt0517@naver.com>
2023-04-26 14:46:56 +0900

    Picker

---
+++ week1/PickerBottomSheetDialog.kt
@@ -0,0 +1,53 @@
+package com.android.hara.presentation.custom
+
+import android.annotation.SuppressLint
+import android.os.Build
+import android.os.Bundle
+import android.view.View
+import androidx.annotation.RequiresApi
+import com.android.hara.R
+import com.android.hara.databinding.FragmentBottomsheetPickerBinding
+import com.android.hara.presentation.base.BindingNotDraggableBottomSheet
+import com.android.hara.presentation.util.HARAobjcet.categoryList
+import dagger.hilt.android.AndroidEntryPoint
+import timber.log.Timber
+
+@AndroidEntryPoint
+class PickerBottomSheetDialog(private val addListener: (Int) -> Unit) :
+    BindingNotDraggableBottomSheet<FragmentBottomsheetPickerBinding>(R.layout.fragment_bottomsheet_picker) {
+
+    private var count = 1
+
+    @SuppressLint("ResourceAsColor")
+    @RequiresApi(Build.VERSION_CODES.Q)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        init()
+        setOnClickListener()
+    }
+
+    private fun init() {
+        with(binding.pickerCategory) {
+            displayedValues = categoryList.toTypedArray()
+            minValue = 0
+            maxValue = categoryList.size - 1
+            displayedValues = categoryList.toTypedArray()
+            wrapSelectorWheel = true //
+        }
+    }
+
+    private fun setOnClickListener() {
+        binding.pickerCategory.setOnValueChangedListener { picker, xoldVal, newVal ->
+            Timber.e(count.toString())
+            count = newVal + 1
+        }
+
+        binding.btnCancel.setOnClickListener {
+            dismiss()
+        }
+        binding.btnComplete.setOnClickListener {
+            addListener(count)
+            dismiss()
+        }
+    }
+}
