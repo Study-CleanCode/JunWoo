d4ca08dc382d273badc9192c4cce3a13a6758738
JunwooKim <rhffldkt0517@naver.com>
2023-05-06 16:28:24 +0900

    Kotlin NamingConvention 예시파일

---
+++ week2/chapter5.kt
@@ -0,0 +1,22 @@
+package.com.my_project
+
+object myCategories
+
+val personComparator = Comparator<Person>
+
+private const val marketLink = "http://play.google.com/store/apps/details?id="
+
+fun my_function
+
+private val sercretpeopleList = mutableListOf<Int>()
+val peopleList :List<Int>  get() = sercretpeopleList
+
+fun myFunction
+
+interface function
+class realFunction : function
+
+fun sample(a:Int, b:Int, c:Int){}
+sample(1,2,3)
+
+val a = (1+2+3)
